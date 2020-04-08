6138de51c75426353dc5a8c8d33c8b40416f3025
jaketrent <trent.jake@gmail.com>
2020-04-08 15:37:28 -0600

    talk index pg

---
+++ src/pages/talk/index.tsx
@@ -0,0 +1,47 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+
+import Logo from "../../ui/logo"
+
+export const query = graphql`
+  query {
+    talk: allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { categories: { in: "Talk" } } }
+    ) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM YYYY")
+            image
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
+
+export default function TalkIndex(props) {
+  return (
+    <main class="meta-list">
+      <a class="meta-list__logo" href="/">
+        <Logo />
+      </a>
+      <h1 class="meta-list__title">Talks</h1>
+      <div class="meta-list__items">
+        <div class="meta-list__count">{props.data.talk.totalCount} talks</div>
+        <div class="meta-list__links">
+          {props.data.talk.edges.map(({ node }) => (
+            <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+          ))}
+        </div>
+      </div>
+    </main>
+  )
+}
