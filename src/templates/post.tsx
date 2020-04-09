6ad4c4e64d7f0222d19fb5718e717a80a255c6e4
jaketrent <trent.jake@gmail.com>
2020-04-08 18:44:01 -0600

    add footer to post

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -91,6 +91,96 @@ export default function PostPage(props) {
         <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/highlight.min.js"></script>
         <script>hljs.initHighlightingOnLoad();</script>
       </main>
+
+      <footer className="single-footer">
+        <div className="single-footer__sky"></div>
+        <div className="grid" aria-hidden="true">
+          <div className="grid__horizon"></div>
+          <div className="grid__deck">
+            <div className="grid__grid-plane">
+              <div className="grid__grid">
+                <div className="grid__grid--vertical">
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                </div>
+                <div className="grid__grid--horizontal">
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                  <div className="grid__grid__line"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="single-footer__content">
+          <div id="disqus_thread"></div>
+          <script src="/js/disqusloader.js"></script>
+          <script>
+            {` disqusLoader('#disqus_thread', { scriptUrl: '//jaketrent.disqus.com/embed.js' }) `}
+          </script>
+
+          <div className="single-footer__disclosures">
+            <Disclosures />
+          </div>
+
+          <ul className="single-footer__list">
+            <li className="single-footer__list__item">
+              {`if (!noted) { prose.licensed = '`}
+              <a href="https://creativecommons.org/licenses/by/3.0/us/">
+                CC 3.0 Attr
+              </a>
+              {`'; code.license = '`}
+              <a href="https://opensource.org/licenses/MIT">MIT</a>
+              {`'`}
+            </li>
+            <li className="single-footer__list__item">
+              {`articles.map(a => ({ ...a, author: '`}
+              <a href="https://keybase.io/jaketrent">Jake Trent</a>
+              {`' }))`}
+            </li>
+            <li className="single-footer__list__item">
+              {`on('contentLike', _ => `}
+              <a href="http://twitter.com/jaketrent">Follow</a>
+              {` || `}
+              <a href="/index.xml">Feed</a>
+              {`)`}
+            </li>
+            <li className="single-footer__list__item">
+              {`on('bugFound', _ => `}
+              <a href="https://github.com/jaketrent/jaketrent-blog/issues/new">
+                Report
+              </a>
+              {`)`}
+            </li>
+            <li className="single-footer__list__item">
+              {`if (agent.isRobot) follow(`}
+              <a href="/sitemap.xml">Sitemap</a>
+              {`)`}
+            </li>
+            <li className="single-footer__list__item">
+              {`console.log(process.env.ENGINE) // "`}
+              <a href="https://gatsbyjs.org">Gatsby</a>
+              {`"`}
+            </li>
+          </ul>
+        </div>
+      </footer>
     </>
   )
 }
@@ -118,3 +208,111 @@ export const query = graphql`
     }
   }
 `
+
+type DisclosureType =
+  | "no-connection"
+  | "affiliate"
+  | "free"
+  | "sponsor"
+  | "employee"
+interface DisclosuresProps {
+  disclosures: DisclosureType[]
+}
+function Disclosures(props) {
+  if (!Array.isArray(props.disclosures)) return null
+
+  return (
+    <div className="single-footer__disclosures">
+      {props.disclosures.some(d => d === "no-connection") && (
+        <p>
+          Disclosure of Material Connection: I have not received any
+          compensation for writing this post. I have no material connection to
+          the brands, products, or services that I have mentioned. I am
+          disclosing this in accordance with the Federal Trade Commission’s{" "}
+          <a
+            href="http://www.access.gpo.gov/nara/cfr/waisidx_03/16cfr255_03.html"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            16 CFR, Part 255
+          </a>
+          : “Guides Concerning the Use of Endorsements and Testimonials in
+          Advertising.”
+        </p>
+      )}
+      {props.disclosures.some(d => d === "affiliate") && (
+        <p className="disclosures__notice">
+          Disclosure of Material Connection: Some of the links in the post above
+          are “affiliate links.” This means if you click on the link and
+          purchase the item, I will receive an affiliate commission. Regardless,
+          I only recommend products or services I use personally and believe
+          will add value to my readers. I am disclosing this in accordance with
+          the Federal Trade Commission’s{" "}
+          <a
+            href="http://www.access.gpo.gov/nara/cfr/waisidx_03/16cfr255_03.html"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            16 CFR, Part 255
+          </a>
+          : “Guides Concerning the Use of Endorsements and Testimonials in
+          Advertising.”
+        </p>
+      )}
+      {props.disclosures.some(d => d === "free") && (
+        <p>
+          Disclosure of Material Connection: I received one or more of the
+          products or services mentioned above for free in the hope that I would
+          mention it on my blog. Regardless, I only recommend products or
+          services I use personally and believe will be good for my readers. I
+          am disclosing this in accordance with the Federal Trade Commission’s{" "}
+          <a
+            href="http://www.access.gpo.gov/nara/cfr/waisidx_03/16cfr255_03.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            16 CFR, Part 255
+          </a>
+          : “Guides Concerning the Use of Endorsements and Testimonials in
+          Advertising.”
+        </p>
+      )}
+      {props.disclosures.some(d => d === "sponsor") && (
+        <p>
+          Disclosure of Material Connection: This is a “sponsored post.” The
+          company who sponsored it compensated me via a cash payment, gift, or
+          something else of value to write it. Regardless, I only recommend
+          products or services I use personally and believe will be good for my
+          readers. I am disclosing this in accordance with the Federal Trade
+          Commission’s{" "}
+          <a
+            href="http://www.access.gpo.gov/nara/cfr/waisidx_03/16cfr255_03.html"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            16 CFR, Part 255
+          </a>
+          : “Guides Concerning the Use of Endorsements and Testimonials in
+          Advertising.”
+        </p>
+      )}
+      {props.disclosures.some(d => d === "employee") && (
+        <p>
+          Disclosure of Material Connection: I am currently an employee of
+          Pluralsight. Regardless, I only recommend books that I have personally
+          read and believe will be good for my readers. I am disclosing this in
+          accordance with the Federal Trade Commission’s{" "}
+          <a
+            href="http://www.access.gpo.gov/nara/cfr/waisidx_03/16cfr255_03.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            16 CFR, Part 255
+          </a>
+          : “Guides Concerning the Use of Endorsements and Testimonials in
+          Advertising.”
+        </p>
+      )}
+    </div>
+  )
+}
