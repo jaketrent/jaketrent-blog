abef652707ec1da89229287e4e5e4590f9ea932d
jaketrent <trent.jake@gmail.com>
2020-05-07 16:57:16 -0600

    rm test cloudfront img

--- src/templates/tag.tsx
+++ src/templates/tag.tsx
@@ -24,7 +24,6 @@ export default function TagPage(props) {
               </Link>
             ))}
           </div>
-          <img src="https://cdn.jaketrent.com/supertest.jpg" />
         </div>
       </main>
     </BlogLayout>
