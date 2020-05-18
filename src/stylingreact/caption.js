db30b9d41cd5a118aa3e9acfb09da69197fbcaba
jaketrent <trent.jake@gmail.com>
2020-05-18 16:06:14 -0600

    add updated banner to styling react

--- src/stylingreact/caption.js
+++ src/stylingreact/caption.js
@@ -2,18 +2,32 @@ import React from "react"
 
 export default _ => (
   <div className="caption">
-    <p>Decide How You'll Style Your React Project</p>
+    <div className="tagline">
+      Decide How You'll Style Your React Project
+      <div className="updated">Updated for 2020!</div>
+    </div>
     <style jsx>{`
       .caption {
-        text-align: center;
-        font-size: 1.3em;
-        text-transform: uppercase;
-        letter-spacing: 1px;
         padding: 40px 0 15px 0;
         border-top: 1px solid #fff;
+        text-transform: uppercase;
       }
-      p {
-        margin: 0;
+      .updated {
+        display: inline-block;
+        background: #61dafb;
+        margin-left: 2rem;
+        color: white;
+        padding: 0.5rem 1rem;
+        border-radius: 3px;
+        text-shadow: -1px -1px 0 rgba(0, 0, 255, 0.5);
+        font-size: 0.7rem;
+      }
+      .tagline {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 1.3em;
+        letter-spacing: 1px;
       }
     `}</style>
   </div>
