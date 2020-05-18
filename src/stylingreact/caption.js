ca478ad8d49a643eb031301c1646db97af516356
jaketrent <trent.jake@gmail.com>
2020-05-18 16:16:31 -0600

    diminish styling react updated tag

--- src/stylingreact/caption.js
+++ src/stylingreact/caption.js
@@ -14,12 +14,11 @@ export default _ => (
       }
       .updated {
         display: inline-block;
-        background: #61dafb;
+        color: #61dafb;
+        border: 1px solid #61dafb;
         margin-left: 2rem;
-        color: white;
         padding: 0.5rem 1rem;
         border-radius: 3px;
-        text-shadow: -1px -1px 0 rgba(0, 0, 255, 0.5);
         font-size: 0.7rem;
       }
       .tagline {
