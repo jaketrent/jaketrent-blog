016e5769595d814676e988b5e5238d27d620cbea
jaketrent <trent.jake@gmail.com>
2020-12-01 16:53:02 -0700

    install emotion deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,8 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-typescript",
+    "gatsby-plugin-emotion",
     "gatsby-plugin-postcss",
-    {
-      resolve: "gatsby-plugin-astroturf",
-      options: {
-        enableCssProp: true,
-      },
-    },
     "gatsby-transformer-remark",
     {
       resolve: `gatsby-source-filesystem`,
