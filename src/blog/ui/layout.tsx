008fb5db3ccbe8cea90de405afe0f1fdfdad3990
jaketrent <trent.jake@gmail.com>
2020-12-24 07:30:54 -0700

    organize common ui files

--- src/blog/ui/layout.tsx
+++ src/blog/ui/layout.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head"
 import React from "react"
 
-import MetaCommon from "../ui/meta-common"
+import MetaCommon from "../../common/ui/meta-common"
 
 interface Props {
   description?: string
