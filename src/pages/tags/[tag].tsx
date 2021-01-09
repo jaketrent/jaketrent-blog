459a5875d99ff0fb689f500947c77ef600f6156a
jaketrent <trent.jake@gmail.com>
2021-01-09 10:18:12 -0700

    convert tag page

--- src/pages/tags/[tag].tsx
+++ src/pages/tags/[tag].tsx
@@ -4,26 +4,44 @@ import { Link, Logo } from "../../common/ui"
 
 import * as css from "../../blog/ui/content-list.module.css"
 
-export async function getStaticProps() {
-  const posts = fetchAllPosts()
+export async function getStaticProps({ params }) {
+  const posts = fetchAllPosts({
+    filterBy: post => post.frontmatter.tags?.includes(params.tag),
+  })
+
   return {
     props: {
+      tag: params.tag,
       posts,
     },
   }
 }
 
-export default function PostIndexPage(props) {
+export const getStaticPaths = () => {
+  const posts = fetchAllPosts()
+  const tags = posts.reduce((tags, post) => {
+    const postTags = Array.isArray(post.frontmatter.tags)
+      ? post.frontmatter.tags.map(tag => ({ params: { tag } }))
+      : []
+    return [...tags, ...postTags]
+  }, [])
+  return {
+    paths: tags,
+    fallback: false,
+  }
+}
+
+export default function TagPage(props) {
   const totalCount = props.posts.length
   return (
-    <BlogLayout title="Posts | Jake Trent">
+    <BlogLayout title={`${props.tag} | Jake Trent`}>
       <main className={css.contentList}>
         <Link href="/">
           <a className={css.logo}>
             <Logo />
           </a>
         </Link>
-        <h1 className={css.title}>Posts</h1>
+        <h1 className={css.title}>{props.tag}</h1>
         <div className={css.items}>
           <div className={css.count}>{totalCount} posts</div>
           <div className={css.links}>
