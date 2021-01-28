b80e64367a39be8a045de3b5464d51575772d9dd
jaketrent <trent.jake@gmail.com>
2021-01-28 06:03:37 -0700

    build script to process for sitemap generation

---
+++ scripts/sitemap/build.sh
@@ -0,0 +1,6 @@
+node ./scripts/sitemap/static.js
+node ./scripts/sitemap/blog.js
+node ./scripts/sitemap/compress.js
+node ./scripts/sitemap/sitemap.js
+#curl http://google.com/ping?sitemap=https://jaketrent.com/sitemap.xml
+
