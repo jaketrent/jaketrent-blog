0411753894f2c3a2383e94a2522223de63c7c843
jaketrent <trent.jake@gmail.com>
2021-01-30 05:45:44 -0700

    add google ping to sitemap build

--- scripts/sitemap/build.sh
+++ scripts/sitemap/build.sh
@@ -2,5 +2,5 @@ npx ts-node -O '{"module":"commonjs"}' ./scripts/sitemap/static.ts
 npx ts-node -O '{"module":"commonjs"}' ./scripts/sitemap/blog.ts
 npx ts-node -O '{"module":"commonjs"}' ./scripts/sitemap/compress.ts
 npx ts-node -O '{"module":"commonjs"}' ./scripts/sitemap/sitemap.ts
-#curl http://google.com/ping?sitemap=https://jaketrent.com/sitemap.xml
+curl http://google.com/ping?sitemap=https://jaketrent.com/sitemap.xml
 
